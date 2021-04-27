3d19c1a82c7e0716754d109b5ed927f1f5cccefd
diego <diego@Portatil>
2021-04-27 12:46:08 +0200

    Añadimos clase Principal

---
+++ Prueba1/src/prueba1/Principal.java
@@ -0,0 +1,12 @@
+package prueba1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("prueba git");
+		
+	}
+}
